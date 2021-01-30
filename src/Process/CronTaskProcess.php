e47e0c2aae8b71982f5496115b8f09f1a77ce411
古俊杰 <ilovintit@gmail.com>
2021-01-30 22:12:34 +0800

    升级单线程后台进程类

--- src/Process/CronTaskProcess.php
+++ src/Process/CronTaskProcess.php
@@ -7,49 +7,74 @@ use Hyperf\Process\AbstractProcess;
 use Iit\HyLib\RedisLock\RedisLock;
 use Exception;
 use Iit\HyLib\Utils\Log;
+use Iit\HyLib\Utils\Str;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class CronTaskProcess
  * @package Iit\HyLib\Process
+ * @method logEmergency($message, array $context = [])
+ * @method logAlert($message, array $context = [])
+ * @method logCritical($message, array $context = [])
+ * @method logError($message, array $context = [])
+ * @method logWarning($message, array $context = [])
+ * @method logNotice($message, array $context = [])
+ * @method logInfo($message, array $context = [])
+ * @method  logDebug($message, array $context = [])
  */
 abstract class CronTaskProcess extends AbstractProcess
 {
+    const SIGNAL_DELAY = 'delay';
     /**
      * @var RedisLock
      */
     public RedisLock $lock;
 
     /**
+     * @var LoggerInterface
+     */
+    public LoggerInterface $logger;
+
+    /**
+     * CronTaskProcess constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->logger = Log::driver();
+        parent::__construct($container);
+    }
+
+    /**
      * The logical of process will place in here.
      */
     public function handle(): void
     {
-        $this->logInfo('start-process-at:' . microtime(true));
+        $this->logInfo('process-started');
         $lockTime = $this->runInterval();
-        $this->logInfo('run-interval-is:' . $lockTime);
+        $this->logInfo('process-interval' . $lockTime);
         if ($lockTime <= 0) {
-            $this->logInfo('run-interval-invalid,after-3600-second-exit:' . microtime(true));
+            $this->logInfo('process-interval-invalid,exit');
             sleep(3600);
             exit;
         }
+        $this->logInfo('process-task-key:' . $this->taskKey());
         $this->lock = RedisLock::create($this->taskKey(), $lockTime);
         do {
-            $this->logInfo('try-to-lock-key:' . microtime(true));
+            $this->logInfo('process-try-lock-key');
             if (!$this->lock->get()) {
-                $this->logInfo('get-lock-failed,sleep:' . microtime(true));
+                $this->logInfo('process-lock-failed,sleep');
                 sleep($this->sleepTime());
                 continue;
             }
-            $this->logInfo('get-lock-successful:' . microtime(true));
+            $this->logInfo('process-lock-successful');
             try {
-                $taskResult = $this->cronTask();
-                if ($taskResult === 'delay') {
-                    sleep($this->sleepTime());
-                }
+                $this->cronTask() === self::SIGNAL_DELAY
+                && sleep($this->sleepTime());
                 $this->lock->release();
             } catch (Exception $exception) {
-                Log::error($exception->__toString());
-                $this->logInfo('run-cron-task-exception:' . microtime(true));
+                $this->logError('process-task-exception', ['exception' => $exception->__toString()]);
                 sleep($this->sleepTime());
                 $this->lock->release();
             }
@@ -65,20 +90,23 @@ abstract class CronTaskProcess extends AbstractProcess
     }
 
     /**
-     * @param $message
-     * @param array $content
-     */
-    protected function logInfo($message, $content = [])
-    {
-        Log::info($this->taskKey() . ':' . $message, $content);
-    }
-
-    /**
+     * 任务识别标识,默认使用类名转换,子类可以覆盖此方法自定义标识
      * @return string
      */
     public function taskKey(): string
     {
-        return str_replace('_', '-', snake_case((new \ReflectionClass(static::class))->getShortName()));
+        return str_replace('_', '-', Str::snakeCase((new \ReflectionClass(static::class))->getShortName()));
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     */
+    public function __call($name, $arguments)
+    {
+        Str::start($name, 'log') &&
+        $this->logger->{str_replace('log', '', $name)}($this->taskKey()
+            . '-' . $arguments[0] . '/' . microtime(true), $arguments[1]);
     }
 
     /**
