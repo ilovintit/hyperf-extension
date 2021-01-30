8da2e91aa1c659fd00c5d6e8d21eac08d8c2db90
古俊杰 <ilovintit@gmail.com>
2021-01-30 18:08:12 +0800

    修复监听器抽象类命名空间

--- src/Listener/AbstractListener.php
+++ src/Listener/AbstractListener.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Iit\HyLib\Contracts;
+namespace Iit\HyLib\Listener;
 
 use Hyperf\Event\Contract\ListenerInterface;
 use Psr\Container\ContainerInterface;
