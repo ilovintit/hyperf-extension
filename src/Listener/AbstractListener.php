7905b5851cd1cb9d96d244716bae42be6c1cee14
古俊杰 <ilovintit@gmail.com>
2021-03-07 09:04:05 +0800

    修复残留注解bug

--- src/Listener/AbstractListener.php
+++ src/Listener/AbstractListener.php
@@ -3,14 +3,12 @@ declare(strict_types=1);
 
 namespace Iit\HyLib\Listener;
 
-use Hyperf\Event\Annotation\Listener;
 use Hyperf\Event\Contract\ListenerInterface;
 use Psr\Container\ContainerInterface;
 
 /**
  * Class AbstractListener
  * @package Iit\HyLib\Contracts
- * @Listener()
  */
 abstract class AbstractListener implements ListenerInterface
 {
