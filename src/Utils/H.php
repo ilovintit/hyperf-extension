79c9b2465c56c8d3e04f4ba5aa86f490c2aa4388
古俊杰 <ilovintit@gmail.com>
2021-03-16 16:57:58 +0800

    增加辅助类

--- src/Utils/H.php
+++ src/Utils/H.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Iit\HyLib\Utils;
 
+use Hyperf\Utils\ApplicationContext;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -17,5 +18,14 @@ class H
     public static function uuid(): string
     {
         return Uuid::uuid4()->toString();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function app($id)
+    {
+        return ApplicationContext::getContainer()->get($id);
     }
 }
